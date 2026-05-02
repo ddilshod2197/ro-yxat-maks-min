de08cf174cff82594b25aaa6eff9f67c389edb25
Dilshod <ddilshod2197@gmail.com>
2026-05-02 16:55:44 +0500

    🚀 Add main application code

---
+++ main.py
@@ -0,0 +1,12 @@
+def tub_sonlar(n):
+    for i in range(2, n + 1):
+        tub = True
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                tub = False
+                break
+        if tub:
+            print(i)
+
+n = int(input("N ni kiriting: "))
+tub_sonlar(n)
